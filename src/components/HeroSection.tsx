af01eb5318c0f0679ad9d016704bf125b626c04d
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-10-30 17:22:55 +0330

    feat: add about up page

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import Image from "next/image";
-import heroImage from "@/assets/images/hero-image.webp"
+import heroImage from "@/assets/images/hero-image.webp";
 
 interface Props {
   heading: string;
   subHeading: string;
+  childHeading: string;
 }
 
-const HeroSection: React.FC<Props> = ({ heading, subHeading }) => {
+const HeroSection: React.FC<Props> = ({
+  heading,
+  subHeading,
+  childHeading,
+}) => {
   return (
     <div className="relative flex h-[500px] items-center justify-center">
-      <Image 
+      <Image
         src={heroImage}
-        alt="Descriptive alt text" 
+        alt="Descriptive alt text"
         className="absolute h-full w-full object-cover"
       />
-      <div className="text-center z-10">
-        <h1 className="text-white mb-4 text-6xl">{heading}</h1>
-        <h2 className="text-gray-400 text-4xl">{subHeading}</h2>
+      <div className="z-10 text-center">
+        <h1 className="mb-4 text-8xl font-extrabold		 text-white	">{heading}</h1>
+        <h2 className="text-3xl font-medium text-gray-400	">
+          {subHeading}{" "}
+          <span className="text-3xl font-medium text-primary">
+            {childHeading}
+          </span>
+        </h2>
       </div>
     </div>
   );
