15a74796595fa69b16f7d7ed297018c3b3c60bf8
AliNikseresht <aliniksereshtdev@gmail.com>
2023-10-31 15:35:25 +0330

    fix:fix map in Cuntact-us

--- src/components/contact-us/Map.tsx
+++ src/components/contact-us/Map.tsx
@@ -9,7 +9,7 @@ declare const process: {
 };
 
 const DEFAULT_MAP_URL =
-  "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d343.41692083310033!2d51.47284865715878!3d35.80365012474445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1698668039594!5m2!1sen!2s";
+  "https://map.ir/lat/35.803776/lng/51.472840/z/16/p/%D9%85%D8%A7%D8%A7%DB%8C%D9%86%D8%AC%D8%A7%DB%8C%DB%8C%D9%85";
 const MAP_URL: string = process.env.NEXT_PUBLIC_MAP_URL || DEFAULT_MAP_URL;
 
 const Map = () => {
