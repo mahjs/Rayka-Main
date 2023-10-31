4054f579a9b5268e814b66aba7510e8c88892db7
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-10-31 15:58:50 +0330

    add new animation for carousel

--- src/components/about-us/Persons.tsx
+++ src/components/about-us/Persons.tsx
@@ -8,16 +8,69 @@ interface Props {}
 
 const Persons: React.FC<Props> = ({}) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const images = [truePerson, truePerson, truePerson];
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const imagesPerPage = 3;
+  const transitionDuration = 800; // Increased duration for a slower, more subtle effect
+
+  const data = [
+    {
+      name: "لیلا",
+      surname: "حسب",
+      images: truePerson,
+    },
+    {
+      name: "نادری",
+      surname: "مکوم",
+      images: truePerson,
+    },
+    {
+      name: "نادری",
+      surname: "مدیسر",
+      images: truePerson,
+    },
+    {
+      name: "نادری",
+      surname: "مدیسر",
+      images: truePerson,
+    },
+    {
+      name: "نادری",
+      surname: "مدیسر",
+      images: cert,
+    },
+    {
+      name: "نادری",
+      surname: "مدیسر",
+      images: truePerson,
+    },
+  ];
 
   const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    if (currentImageIndex < data.length / imagesPerPage - 1) {
+      setIsTransitioning(true); // Start the animation
+      setTimeout(() => {
+        setCurrentImageIndex((prev) => prev + 1);
+        setIsTransitioning(false); // End the animation
+      }, 300); // Change after 300ms
+    }
   };
+
   const prevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
-    );
+    if (currentImageIndex > 0) {
+      setIsTransitioning(true); // Start the animation
+      setTimeout(() => {
+        setCurrentImageIndex((prev) => prev - 1);
+        setIsTransitioning(false); // End the animation
+      }, 300); // Change after 300ms
+    }
   };
+
+  const visibleData = data.slice(
+    currentImageIndex * imagesPerPage,
+    (currentImageIndex + 1) * imagesPerPage,
+  );
+
   return (
     <div className="flex flex-col items-center justify-center">
       <p className="mb-[4rem] text-7xl font-extrabold">خانواده ی رایکا</p>
@@ -27,34 +80,25 @@ const Persons: React.FC<Props> = ({}) => {
           className="relative w-full"
           data-carousel="static"
         >
-          <div className="relative  flex h-56 w-[78rem] justify-center gap-9 overflow-hidden rounded-lg md:h-[40rem]">
-            <div className="group relative flex flex-col items-center justify-center ">
-              <Image
-                src={images[currentImageIndex]}
-                alt="truePerson"
-                className="mb-9 scale-100 transform rounded-2xl grayscale transition-transform duration-500 group-hover:scale-110 group-hover:grayscale-0"
-              />
-              <p className="text-4xl font-bold">لیلا نادری</p>
-              <p className="text-base font-medium">مدیر</p>
-            </div>{" "}
-            <div className="group relative flex flex-col items-center justify-center">
-              <Image
-                src={images[currentImageIndex]}
-                alt="truePerson"
-                className="mb-9 scale-100 transform rounded-2xl  grayscale transition-transform duration-500 group-hover:scale-110 group-hover:grayscale-0"
-              />
-              <p className="text-4xl font-bold">لیلا نادری</p>
-              <p className="text-base font-medium">مدیر</p>
-            </div>{" "}
-            <div className="group relative flex flex-col items-center justify-center">
-              <Image
-                src={images[currentImageIndex]}
-                alt="truePerson"
-                className="mb-9 scale-100 transform rounded-2xl  grayscale transition-transform duration-500 group-hover:scale-110 group-hover:grayscale-0"
-              />
-              <p className="text-4xl font-bold">لیلا نادری</p>
-              <p className="text-base font-medium">مدیر</p>
-            </div>
+          <div
+            className={`relative flex h-56 w-[78rem] max-w-7xl justify-center gap-9 overflow-hidden rounded-lg transition-all duration-300 md:h-[40rem] ${
+              isTransitioning ? "translate-x-[-100%] transform" : ""
+            }`}
+          >
+            {visibleData.map((item, index) => (
+              <div
+                key={index}
+                className="group relative flex flex-col items-center justify-center"
+              >
+                <Image
+                  src={item.images}
+                  alt="truePerson"
+                  className="mb-9 scale-100 transform rounded-2xl grayscale transition-transform duration-500 group-hover:scale-110 group-hover:grayscale-0"
+                />
+                <p className="text-4xl font-bold">{item.name}</p>
+                <p className="text-base font-medium">{item.surname}</p>
+              </div>
+            ))}
           </div>
           <button
             onClick={prevImage}
@@ -86,7 +130,7 @@ const Persons: React.FC<Props> = ({}) => {
             className="group absolute right-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
             onClick={nextImage} // Call the nextImage function on button click
           >
-            <span className="mb-[6rem] mr-8 inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
+            <span className="mb-[7rem] mr-8 inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
               <svg
                 className="h-4 w-4 text-white dark:text-gray-800"
                 aria-hidden="true"
