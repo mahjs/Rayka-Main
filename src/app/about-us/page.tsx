6595687fb8628f8c7f44202a75f7407fe6cc44eb
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-02 12:00:42 +0330

    feat: add one section for responsive of the about page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -16,21 +16,17 @@ const page: React.FC<Props> = ({}) => {
         childHeading="درباره ما"
       />
       <div className="relative mb-[10rem] mt-[6.69rem] flex justify-center  ">
-        <div className="absolute h-[80%] w-[70.3%] -rotate-[2.5deg]  rounded-3xl bg-[#FEA918]" />
-        <Image
-          src={AboutUs}
-          alt="About us"
-          className="relative h-[80%] w-[70%]  rounded-3xl"
-        />
-        <div className="absolute z-20 flex  h-[80%] w-[70%] flex-col	 gap-y-4	 rounded-3xl px-[7.8rem] pt-[2.78rem]">
+        <div className="absolute  h-8 w-11/12  -rotate-[2.5deg] rounded-3xl bg-[#FEA918] md:h-80 md:w-3/5" />
+        <div className="relative flex w-11/12 flex-col  gap-2 rounded-3xl bg-black px-[2.69rem] py-[2rem] md:h-96 md:w-3/5 md:gap-4 md:px-[7.8rem]	 md:py-[2.78rem]">
           <Image
             src="/images/rayka-icon.svg"
             width="50"
             height="50"
             alt="rayka icon"
+            className="w-12 md:w-16 "
           />
           <p className="  text-5xl font-bold text-primary	">درباه‌ما </p>
-          <p className="  text-[1rem] font-medium	 text-white">
+          <p className="  mt-3 text-justify	 text-[1rem] font-medium text-white	">
             ما در خانواده ی سخت کوش و خستگی ناپذیر شرکت ماورای ابعاد رایکا ،
             برای تحقق رویاهایمان میجنگیم. در کنار هم میسازیم و از تماشای آنچه
             روزی تصور کرده بودیم و حالا به آن تجسم بخشیده ایم لذت میبریم. لورم
@@ -41,7 +37,7 @@ const page: React.FC<Props> = ({}) => {
             در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را
             می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
             الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
-          </p>
+          </p>{" "}
         </div>
       </div>
       <Honers />
---
+++ src/app/dist/layout.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.metadata = void 0;
+require("./globals.css");
+exports.metadata = {
+    title: "ماورای ابعاد رایکا",
+    description: "همگام با تکنولوژی برای آینده ای هوشمندتر"
+};
+function RootLayout(_a) {
+    var children = _a.children;
+    return (React.createElement("html", { lang: "en", dir: "rtl" },
+        React.createElement("body", null, children)));
+}
+exports["default"] = RootLayout;
