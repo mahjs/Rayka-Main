a6f5168a58a8e3cb742ebf49f8cd9ef61d17c26f
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-05 13:37:32 +0330

    handle the person carousel

--- src/components/about-us/Carousel.tsx
+++ src/components/about-us/Carousel.tsx
@@ -17,44 +17,45 @@ const Carousel: React.FC<CarouselProps> = ({ children, active, setActive }) => {
 
   return (
     <div className="carousel">
-      {/* {active > 0 && (
-        <button
-          type="button"
-          aria-label="Next"
-          className="nav left"
-          onClick={() => setActive((i: number) => i - 1)}
-        >
-          <Image src={arrowLeft} alt="arrow left" />
-        </button>
-      )} */}
-      {/* @ts-ignore */}
-      {React.Children.map(children, (child: ReactElement, i: number) => (
-        <div
-          className="card-container"
-          style={{
-            // @ts-ignore
-            "--active": i === active ? 1 : 0,
-            "--offset": (active - i) / 3,
-            "--direction": Math.sign(active - i),
-            "--abs-offset": Math.abs(active - i) / 3,
-            "pointer-events": active === i ? "auto" : "none",
-            opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
-            display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
-          }}
-        >
-          {child}
-        </div>
-      ))}
-      {/* {active < count - 1 && (
-        <button
-          aria-label="Next"
-          type="button"
-          className="nav right"
-          onClick={() => setActive((i: number) => i + 1)}
-        >
-          <Image src={arrowRight} alt="arrow right" />
-        </button>
-      )} */}
+      <div className="flex h-full w-96 items-center justify-evenly">
+        {active > 0 && (
+          <button
+            type="button"
+            aria-label="Next"
+            className="nav left"
+            onClick={() => setActive((i: number) => i - 1)}
+          >
+            <Image src={arrowLeft} alt="arrow left" />
+          </button>
+        )}
+        {React.Children.map(children, (child: ReactElement, i: number) => (
+          <div
+            className="card-container"
+            style={{
+              // @ts-ignore
+              "--active": i === active ? 1 : 0,
+              "--offset": (active - i) / 3,
+              "--direction": Math.sign(active - i),
+              "--abs-offset": Math.abs(active - i) / 3,
+              "pointer-events": active === i ? "auto" : "none",
+              opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+              display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+            }}
+          >
+            {child}
+          </div>
+        ))}
+        {active < count - 1 && (
+          <button
+            aria-label="Next"
+            type="button"
+            className="nav right"
+            onClick={() => setActive((i: number) => i + 1)}
+          >
+            <Image src={arrowRight} alt="arrow right" />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
