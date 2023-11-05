3a1f573473c269c519afc9abb36fa68e903adb38
ali nikseresht <alinksht@gmail.com>
2023-11-05 17:17:21 +0330

    fix: fix map

--- src/app/contact-us/_Components/Map.tsx
+++ src/app/contact-us/_Components/Map.tsx
@@ -25,7 +25,7 @@ const DEFAULT_MAP_URL =
   "https://map.ir/lat/35.803776/lng/51.472840/z/16/p/%D9%85%D8%A7%D9%8A%D9%86%D8%AC%D8%A7%DB%8C%DB%8C%D9%85";
 const MAP_URL: string = process.env.NEXT_PUBLIC_MAP_URL || DEFAULT_MAP_URL;
 
-const Map: React.FC<MapProps> = ({ latitude, longitude, companyName }) => {
+const Map: React.FC<MapProps> = ({ latitude, longitude }) => {
   const [showModal, setShowModal] = useState(false);
   const isMobile = (): boolean =>
     /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
@@ -57,9 +57,7 @@ const Map: React.FC<MapProps> = ({ latitude, longitude, companyName }) => {
         break;
       case NavigationService.Google:
       default:
-        url = `geo:${latitude},${longitude}?q=${encodeURIComponent(
-          companyName,
-        )}`;
+        url = `geo:${latitude},${longitude}`;
         if (!isMobile()) {
           url = "https://maps.app.goo.gl/B2jSX3MbPCfWDPo99";
         }
