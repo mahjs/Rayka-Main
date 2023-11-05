e4550f30ebdb398f376db693dd3e8405de1ba377
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-05 13:07:26 +0330

    handle the ipad responsive

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -21,7 +21,7 @@ const page: React.FC<Props> = ({}) => {
           style={{
             backgroundSize: "100%",
           }}
-          className="relative h-[22rem] w-[90%] flex-col items-center justify-center rounded-lg bg-main-image md:h-[23rem] md:w-[80%] md:rounded-3xl"
+          className="relative h-[22rem] w-[90%] flex-col items-center justify-center rounded-lg bg-main-image md:h-[50dvh] md:w-[80%] md:rounded-3xl"
         >
           <div className="absolute bottom-0 left-0 right-0 top-0 z-10 h-full w-full rounded-3xl bg-black bg-opacity-30" />
         </div>
