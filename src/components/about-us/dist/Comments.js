beef3663c0e7c989c19e38f09b99f50c1c71f1a6
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-05 10:03:38 +0330

    handle the carousel

--- src/components/about-us/dist/Comments.js
+++ src/components/about-us/dist/Comments.js
@@ -10,24 +10,24 @@ var Comments = function (_a) {
     var _b = react_1.useState(2), active = _b[0], setActive = _b[1];
     var cardsData = [
         {
-            text: "dhsaskjli",
-            name: "hossein",
-            job: "backend"
+            text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد",
+            name: "امیرحسین",
+            job: "فرانت-اند"
         },
         {
-            text: "amitkhodaaa",
-            name: "reza",
-            job: "ui/ux"
+            text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد",
+            name: "مهدی",
+            job: "فرانت-اند"
         },
         {
-            text: "kdmlkdlk",
-            name: "sina",
-            job: "devops"
+            text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد",
+            name: "مهدی",
+            job: "فرانت-اند"
         },
     ];
     return (react_1["default"].createElement("div", { className: "relative mb-[14.35rem] flex h-[500px] items-start justify-center " },
         react_1["default"].createElement(image_1["default"], { src: hero_image_webp_1["default"], alt: "Descriptive alt text", className: "absolute h-full w-full object-cover" }),
-        react_1["default"].createElement("div", { className: "z-10 flex w-full flex-col items-center justify-start gap-10 py-5" },
+        react_1["default"].createElement("div", { className: "z-10 flex w-full flex-col items-center justify-start gap-2 py-2" },
             react_1["default"].createElement("h1", { className: "title text-white" }, "\u0635\u062D\u0628\u062A \u0645\u062F\u06CC\u0631\u0627\u0646"),
             react_1["default"].createElement(Carousel_1["default"], { active: active, setActive: setActive }, cardsData.map(function (card, i) { return (react_1["default"].createElement(CommentsCard_1.CommentsCard, { key: i, text: card.text, name: card.name, job: card.job, isActive: i === active })); })))));
 };
