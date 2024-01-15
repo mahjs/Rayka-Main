d821f90e74cb334d00bb2d72330708f150e5a74e
AliNikseresht <alinksht@gmail.com>
2024-01-15 12:43:29 +0330

    style: fix number and responsive in mobile device in address components

--- src/app/contact-us/_Components/Address.tsx
+++ src/app/contact-us/_Components/Address.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Address = () => {
     return (
         <div className='flex justify-center items-center'>
-            <ul className='flex justify-center md:justify-between items-center w-[75rem] flex-wrap'>
+            <ul className='flex justify-center md:justify-between items-center w-[15rem] md:w-[75rem] flex-wrap px-[2em] mt-[2em] md:mt-0'>
                 <li className='flex flex-col justify-center items-center text-[1.5rem] font-[800] text-center'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 37 29" fill="none">
                         <path d="M2.37842 7.11667L16.8931 16.7931C17.4977 17.1965 18.2083 17.4118 18.9351 17.4118C19.6619 17.4118 20.3725 17.1965 20.9771 16.7931L35.4918 7.11667M6.05768 27.3526H31.8125C32.7883 27.3526 33.7242 26.965 34.4142 26.275C35.1042 25.585 35.4918 24.6492 35.4918 23.6734V5.27704C35.4918 4.30124 35.1042 3.36541 34.4142 2.67541C33.7242 1.98541 32.7883 1.59778 31.8125 1.59778H6.05768C5.08188 1.59778 4.14605 1.98541 3.45605 2.67541C2.76605 3.36541 2.37842 4.30124 2.37842 5.27704V23.6734C2.37842 24.6492 2.76605 25.585 3.45605 26.275C4.14605 26.965 5.08188 27.3526 6.05768 27.3526Z" stroke="#FEA918" stroke-width="2.78014" stroke-linecap="round" stroke-linejoin="round" />
@@ -11,12 +11,14 @@ const Address = () => {
                     پست الکترونیکی
                     <span className='text-[#878787] font-[400] text-[1rem]'>support@raikaco.org</span>
                 </li>
-                <li className='flex flex-col justify-center items-center text-[1.5rem] font-[800] text-center'>
+                <li className='flex flex-col justify-center items-center text-[1.5rem] font-[800] text-center my-[2em] md:my-[0]'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 37 37" fill="none">
                         <path d="M20.3484 2C20.3484 2 24.386 2.36706 29.5248 7.50587C34.6636 12.6447 35.0307 16.6823 35.0307 16.6823M20.7283 8.48958C20.7283 8.48958 22.5452 9.00713 25.2706 11.7325C27.996 14.4579 28.5154 16.2749 28.5154 16.2749M13.0769 8.08582L14.2681 10.2203C15.3417 12.1473 14.9104 14.6727 13.2183 16.3666C13.2183 16.3666 11.1627 18.4203 14.8884 22.1441C18.6085 25.8643 20.664 23.8142 20.664 23.8142C22.358 22.1203 24.8852 21.689 26.8104 22.7626L28.9449 23.9556C31.8538 25.578 32.197 29.656 29.6404 32.2144C28.1043 33.7487 26.2213 34.9453 24.1419 35.0224C20.6402 35.1563 14.692 34.2699 8.72548 28.3052C2.76079 22.3387 1.87434 16.3905 2.00832 12.8888C2.08724 10.8094 3.28201 8.92638 4.81631 7.39024C7.3747 4.83369 11.4527 5.17689 13.0751 8.08765L13.0769 8.08582Z" stroke="#FEA918" stroke-width="2.78014" stroke-linecap="round" />
                     </svg>
                     شماره تماس
-                    <span className='text-[#878787] font-[400] text-[1rem] direc'>+21 3745 41 12</span>
+                    <span className='text-[#878787] font-[400] text-[1rem]'>
+                        12 41 3745 21+
+                    </span>
                 </li>
                 <li className='flex flex-col justify-center items-center text-[1.5rem] font-[800] text-center'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 36 36" fill="none">
