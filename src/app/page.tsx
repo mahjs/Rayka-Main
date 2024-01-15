6d3a082e2a1058ca0da70ad23dbc7cc31457530b
mahjs <mahjsdev@gmail.com>
2024-01-15 12:25:49 +0330

    fix ui issues and responsivness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
           style={{
             backgroundSize: "100%",
           }}
-          className="relative h-[24rem] w-[90%] flex-col items-center justify-center rounded-lg bg-work-plans-image bg-cover bg-center md:h-[40rem] md:w-[80%] md:rounded-3xl lg:h-[18rem] lg:w-[90%]"
+          className="relative h-[24rem] w-[90%] flex-col items-center justify-center rounded-lg bg-work-plans-image bg-cover bg-center brightness-[.2] md:h-[40rem] md:w-[80%] md:rounded-3xl lg:h-[18rem] lg:w-[90%]"
         >
           <div className="absolute bottom-0 left-0 right-0 top-0 z-10 h-full w-full rounded-3xl bg-black bg-opacity-30" />
         </div>
@@ -94,13 +94,13 @@ export default function Home() {
 
       {/* Information Content */}
       <section
-        className="relative flex h-[20rem] w-full items-center justify-center bg-statistics-image bg-cover bg-center md:mb-[5rem]"
+        className="relative flex h-[20rem] w-full items-center justify-center bg-statistics-image bg-cover bg-center md:mb-[5rem] lg:mt-16"
         style={{
           backgroundSize: "100%",
         }}
       >
-        <div className="absolute bottom-0 left-0 right-0 top-0 z-10 h-full w-full rounded-3xl bg-black bg-opacity-60" />
-        <div className="absolute z-20 grid w-full grid-cols-2 justify-evenly gap-5 md:flex ">
+        <div className="absolute bottom-0 left-0 right-0 top-0 z-10 h-full w-full rounded-3xl bg-black bg-opacity-80" />
+        <div className="absolute z-30 grid w-full grid-cols-2 justify-evenly gap-5 md:flex ">
           {informationData.map((info) => (
             <InfoText
               key={info.id}
